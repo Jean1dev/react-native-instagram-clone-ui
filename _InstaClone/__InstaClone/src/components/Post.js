32377a70f03d6faeaed306ccfb58a33da6006cbd
Jean <jeanlucafp@gmail.com>
2019-02-22 11:31:17 -0300

    correcao nos comments

--- _InstaClone/__InstaClone/src/components/Post.js
+++ _InstaClone/__InstaClone/src/components/Post.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { StyleSheet, View, Image, Dimensions } from 'react-native'
+import { connect } from 'react-redux'
 
 import Author from './Author'
 import Comments from './Comments'
@@ -7,13 +8,15 @@ import AddComment from './AddComment'
 
 class Post extends Component {
     render() {
+        const _addComment = this.props.name ?
+            <AddComment postId={this.props.id} /> : null
         return (
             <View style={styles.container}>
                 <Image source={this.props.image} style={styles.image} />
                 <Author email={this.props.email}
                     nickname={this.props.nickname} />
                 <Comments comments={this.props.comments} />
-                <AddComment postId={this.props.id} />
+                {_addComment}
             </View>
         )
     }
@@ -30,4 +33,10 @@ const styles = StyleSheet.create({
     }
 })
 
-export default Post
+const mapStateToProps = ({ user }) => {
+    return {
+        name: user.name
+    }
+}
+
+export default connect(mapStateToProps)(Post)
